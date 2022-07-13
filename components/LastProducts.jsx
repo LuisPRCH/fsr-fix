91a93db2ed749a6551e23b5aa6afb472e7c41c5e
Luis <eje@eje.com>
2022-07-13 14:31:07 -0600

    fix sizes home page

--- components/LastProducts.jsx
+++ components/LastProducts.jsx
@@ -2,7 +2,7 @@ import { CardOfProduct, TitleOfSection } from '.'
 
 const LastProducts = ({ ProductsData }) => {
 	return (
-		<section className='w-full min-h-[500px] py-8 px-8 flex flex-col justify-start items-center gap-8 xl:px-44'>
+		<section className='w-full min-h-[500px] py-8 px-8 flex flex-col justify-start items-center gap-8'>
 			<TitleOfSection title='Últimos Productos' />
 			<article className='flex flex-wrap w-full justify-center items-center gap-8'>
 				<CardOfProduct cardToMake={ProductsData} />
